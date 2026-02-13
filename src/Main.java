72447dc8c08de7cc2e07b6676e1b79aea321b204
mm541145 <moazzamparwez40@gmail.com>
2026-02-13 20:11:27 +0530

    created(thread-lifecycle):Program of Thread-lifecycle BLOCKED and TIMED_WAITING

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,33 @@
 import jdk.jfr.Threshold;
 
+import static java.lang.Thread.sleep;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main() {
-        Thread t = new Thread (() -> {
-            for(int i=0; i<1000000 ; i++);
-        });
-        System.out . println( t . getState () );
-        t.start();
-        while(t.getState() != Thread.State.TERMINATED) {
-            System.out.println(t.getState());
-        }
+        Object obj = new Object();
+        Thread t1 = new Thread(() -> {
+            synchronized (obj) {
+                for(long i=0;i < 100000000000L; i++);
+            }
+        }, "t1");
+        Thread t2 = new Thread(() -> {
+            try {
+                sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            synchronized (obj) {
+                for(int i=0;i < 100000000; i++);
+            }
+        }, "t2");
+        t1.start();
+        t2.start();
 
+        while(t1.isAlive() || t2.isAlive()) {
+            System.out.println(t1.getName()+" "+t1.getState());
+            System.out.println(t2.getName()+" "+t2.getState());
+        }
     }
 }
