16d33e6da055b5752d70a66a2c5a1915babce58f
mm541145 <moazzamparwez40@gmail.com>
2026-02-13 20:24:05 +0530

    created(thread-lifecycle):Program of Thread-lifecycle Join and WAITING

--- src/Main.java
+++ src/Main.java
@@ -5,29 +5,21 @@ import static java.lang.Thread.sleep;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main() {
-        Object obj = new Object();
-        Thread t1 = new Thread(() -> {
-            synchronized (obj) {
-                for(long i=0;i < 100000000000L; i++);
+    public static void main() throws InterruptedException {
+        Thread mainThread = Thread.currentThread();
+        Thread t = new Thread(() -> {
+            for(int i=0;i < 10; i++) {
+                System.out.println("main: "+" "+mainThread.getState()+" t: "+Thread.currentThread().getState());
+                try {
+                    sleep(500);
+                } catch (InterruptedException e) {
+                }
             }
-        }, "t1");
-        Thread t2 = new Thread(() -> {
-            try {
-                sleep(100);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            synchronized (obj) {
-                for(int i=0;i < 100000000; i++);
-            }
-        }, "t2");
-        t1.start();
-        t2.start();
+        });
 
-        while(t1.isAlive() || t2.isAlive()) {
-            System.out.println(t1.getName()+" "+t1.getState());
-            System.out.println(t2.getName()+" "+t2.getState());
-        }
+        t.start();
+        t.join();
+        System.out.println("main: "+mainThread.getState()+" t:"+t.getState());
+
     }
 }
